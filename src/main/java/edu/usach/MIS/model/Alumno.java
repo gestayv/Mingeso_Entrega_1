9facb256f175fe401b2d9f1efc868e3949a6e3dc
ichigohollow <gestayv@hotmail.es>
2017-09-19 21:29:34 -0300

    Get por id arreglado, cambios en acceso a la bd

--- src/main/java/edu/usach/MIS/model/Alumno.java
+++ src/main/java/edu/usach/MIS/model/Alumno.java
@@ -10,7 +10,7 @@ public class Alumno {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
+    private Long id;
     
     private Integer rut;
     
@@ -24,11 +24,11 @@ public class Alumno {
     
     private Integer ingreso;
     
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
