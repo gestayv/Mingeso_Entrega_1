bec1c103c5ea62fd87b194e709c94ae65fdb1e1b
ichigohollow <gestayv@hotmail.es>
2017-09-21 01:17:52 -0300

    CORS habilitado

--- src/main/java/edu/usach/MIS/MisApplication.java
+++ src/main/java/edu/usach/MIS/MisApplication.java
@@ -2,11 +2,18 @@ package edu.usach.MIS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MisApplication {
+public class MisApplication extends SpringBootServletInitializer{
 
-	public static void main(String[] args) {
-		SpringApplication.run(MisApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MisApplication.class);
+    }
+    
+    public static void main(String[] args) {
+            SpringApplication.run(MisApplication.class, args);
+    }
 }
